f4306ef1a6ac6d978c4a1b934ac03a541bd20ef6
tobolu <chmp287@gmail.com>
2020-10-01 06:15:59 -0700

    execise

---
+++ index.js
@@ -0,0 +1,24 @@
+
+// function yearUntilRetirement(year, firstName) {
+//   var age = 28;
+//   var retirement = 65 - age;
+//   if (retirement > 0) {
+//     console.log(firstName + "Retired in" + retirement + "years");
+//   } else {
+//     console.log(firstName + "is allready retired.");
+//   }
+// }
+
+yearUntilRetirement= (year, firstName) => {
+  let age = 28;
+  let retirement = 65 - age;
+  if (retirement > 0) {
+    console.log(firstName + "Retired in" + retirement + "years");
+  } else {
+    console.log(firstName + "is allready retired.");
+  }
+}
+
+
+
+
